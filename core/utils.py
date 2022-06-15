c8cd32fc53379da3233f64da8866fe2d5c44216a
faltunik <niksanat8@gmail.com>
2022-06-15 17:50:16 +0530

    tried to delete task if it get failed, butthis appraoch failed

--- core/utils.py
+++ core/utils.py
@@ -1,11 +1,25 @@
 import subprocess
 
-def speed_audio(audio_name, input, speed=3):
+from celery import shared_task 
+
+from .manager import task_lock
+
+@shared_task(bind=True)
+def speed_audio(self,audio_name, input, speed=3):
     """
     Speed up the audio
     """
-    output = f"./audiox/{audio_name}_speed.mp3"
-    s = f"ffmpeg -i {input} -filter:a atempo={speed} {output}"
-    cmd = s.split(" ")
-    subprocess.check_output(cmd)
-    return output
+
+    task_id = self.request.id
+    try:
+        output = f"./audiox/{audio_name}_speed.mp3"
+        s = f"ffmpeg -i {input} -filter:a atempo={speed} {output}"
+        cmd = s.split(" ")
+        subprocess.check_output(cmd)
+        return output
+    except Exception as e:
+        if self.AsyncResult(self.request.id).state == "FAILURE":
+            print("task failed")
+            self.revoke(self.request.id, terminate=True)
+        print(e)
+        return None
