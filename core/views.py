8a897ac071584ded94fea30be50ad28edd4529a4
faltunik <niksanat8@gmail.com>
2022-06-15 16:51:09 +0530

    removed print statement

--- core/views.py
+++ core/views.py
@@ -1,20 +1,32 @@
-from django.shortcuts import render
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
+
 from .serializer import AudioSerializer
+from .utils import speed_audio
+from .models import Audio
+
+
+
 
 # Create your views here.
 
 @api_view(['POST'])
 def audio_speed(request):
     serializer = AudioSerializer(data=request.data)
-    print(request.data)
-    print(request.data['audio'].name)
-    print(type(request.data['audio']))
-    print(request.data['speed'])
     if serializer.is_valid():
-        # serializer.save()
+        audio_ins = serializer.save()
+        audio = Audio.objects.get(id=audio_ins.id)
         print("audio is getting processed")
+        print(audio.audio.name)
+        print(audio.audio.path)
+        speedex = speed_audio(audio.audio.name.replace("/", ""), audio.audio, request.data['speed'] )
+        print("audio processed")
+        serializer.save(audio_speed = speedex)
         return Response(serializer.data, status=201)
     return Response(serializer.errors, status=400)
+
+
+
+
+
