c69539a38892c48e9635d8bc0133ab9f4902181a
faltunik <niksanat8@gmail.com>
2022-06-15 18:01:32 +0530

    now executing task using the context manager

--- core/utils.py
+++ core/utils.py
@@ -3,23 +3,20 @@ import subprocess
 from celery import shared_task 
 
 from .manager import task_lock
+from .models import Audio
 
 @shared_task(bind=True)
-def speed_audio(self,audio_name, input, speed=3):
+def speed_audio(self,audio_id, input, speed=3):
     """
     Speed up the audio
     """
-
-    task_id = self.request.id
-    try:
-        output = f"./audiox/{audio_name}_speed.mp3"
-        s = f"ffmpeg -i {input} -filter:a atempo={speed} {output}"
-        cmd = s.split(" ")
-        subprocess.check_output(cmd)
-        return output
-    except Exception as e:
-        if self.AsyncResult(self.request.id).state == "FAILURE":
-            print("task failed")
-            self.revoke(self.request.id, terminate=True)
-        print(e)
-        return None
+    taskid = self.request.id
+    with task_lock("task-lock", taskid , lock_expire_seconds=10) as acquired:
+        if acquired:
+            audio_ins = Audio.objects.get(id = audio_id)
+            output = f"./audiox/{audio_ins.name}_speed.mp3"
+            s = f"ffmpeg -i {input} -filter:a atempo={speed} {output}"
+            cmd = s.split(" ")
+            subprocess.check_output(cmd)
+            audio_ins.audio_speed = output
+            audio_ins.save(update_fields = ['audio_speed'])
