80e75dc1fc5b631c2f07fa12a975c4491d6729cc
faltunik <niksanat8@gmail.com>
2022-06-15 16:29:11 +0530

    changed  the serializer parent class to implement create mthod auto

--- core/serializer.py
+++ core/serializer.py
@@ -1,7 +1,7 @@
 from rest_framework import serializers
 from .models import Audio
 
-class AudioSerializer(serializers.Serializer):
+class AudioSerializer(serializers.ModelSerializer):
    """
    Audio Serializer
    """
