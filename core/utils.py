a41ad6f55fa2de8b1e7dbe5c5805b176abfe6733
faltunik <niksanat8@gmail.com>
2022-06-15 16:43:12 +0530

    changed output loacation, as ffmpeg doesn't create folder

--- core/utils.py
+++ core/utils.py
@@ -1,10 +1,10 @@
 import subprocess
 
-def speed_audio(audio_name, input, speed=2):
+def speed_audio(audio_name, input, speed=3):
     """
     Speed up the audio
     """
-    output = f".media/{audio_name}_speed.mp3"
+    output = f"./audiox/{audio_name}_speed.mp3"
     s = f"ffmpeg -i {input} -filter:a atempo={speed} {output}"
     cmd = s.split(" ")
     subprocess.check_output(cmd)
