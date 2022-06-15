2fdba52446cf990b9d4deb7bbb0991b23c339045
faltunik <niksanat8@gmail.com>
2022-06-15 14:36:01 +0530

    added view logic to test serializer is working or not

--- core/views.py
+++ core/views.py
@@ -1,1 +1,16 @@
+from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
+from .serializer import AudioSerializer
+
+# Create your views here.
+
+@api_view(['POST'])
+def audio_speed(request):
+    serializer = AudioSerializer(data=request.data)
+    print(request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data, status=201)
+    return Response(serializer.errors, status=400)
