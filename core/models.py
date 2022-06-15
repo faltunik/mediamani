17a1ef807e43fb38f907aa9b5e4bccb9061bd519
faltunik <niksanat8@gmail.com>
2022-06-15 15:15:40 +0530

    added audio model

--- core/models.py
+++ core/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Audio(models.Model):
+    audio = models.FileField(upload_to='audio/')
+    speed = models.FloatField()
+    audio_speed = models.FileField(upload_to='audio_x/', blank=True, null= True)
+    
+    def __str__(self):
+        return self.audio.name
