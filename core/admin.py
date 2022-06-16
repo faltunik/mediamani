530441b4adf32c2f2a53c85a970b8fba99c95b96
faltunik <niksanat8@gmail.com>
2022-06-16 19:54:27 +0530

    register video extract model

--- core/admin.py
+++ core/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Audio
+from .models import Audio, Video_Extract
 
 # Register your models here.
 admin.site.register(Audio)
+admin.site.register(Video_Extract)
