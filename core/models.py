8145d491bc89ff64a1c1fe4e8918bda733377c6a
faltunik <niksanat8@gmail.com>
2022-06-16 19:53:22 +0530

    added videoextract model to extract audio from video

--- core/models.py
+++ core/models.py
@@ -2,7 +2,7 @@ import uuid
 from django.db import models
 from django.db.models.signals import post_save
 
-from .utils import speed_audio
+from .utils import speed_audio, extract_audio
 
 # Create your models here.
 
@@ -19,3 +19,16 @@ def speedup_audio(sender,instance,created,**kwargs):
 
 
 post_save.connect(speedup_audio,sender=Audio,dispatch_uid=uuid.uuid4)
+
+class Video_Extract(models.Model):
+    video = models.FileField(upload_to='video/')
+    audio_speed = models.FileField(upload_to='audio_extract/', blank=True, null= True)
+    
+    def __str__(self):
+        return self.video.name
+
+def audio_extract(sender,instance,created,**kwargs):
+    extract_audio.delay(video_id=instance.id, input=instance.audio.path)
+
+
+post_save.connect(audio_extract,sender=Video_Extract,dispatch_uid=uuid.uuid4)
