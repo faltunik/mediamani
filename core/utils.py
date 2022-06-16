e1d448809a2fe7bdf271fa8e7c2ad988b59bf855
faltunik <niksanat8@gmail.com>
2022-06-16 20:33:23 +0530

    added function that extract audio

--- core/utils.py
+++ core/utils.py
@@ -24,3 +24,24 @@ def speed_audio(self,audio_id, input, speed=3):
             subprocess.check_output(cmd)
             audio_ins.audio_speed = output
             audio_ins.save(update_fields = ['audio_speed'])
+
+
+# cmd = ['ffmpeg', '-i', 'inputvid.mp4', f'demoaudio{a}.mp3']
+
+@shared_task(bind=True)
+def extract_audio(self,video_id, input):
+    taskid = self.request.id
+    with task_lock("task-lock", taskid , lock_expire_seconds=10) as acquired:
+        if acquired:
+            video_ins = m.Video_Extract.objects.get(id = video_id)
+            print("------------------------------------------------------------------")
+            print("file name or path is: ", video_ins.video.name)
+            rename = str(video_ins.video.name).replace("/", "_")
+            print("------------------------------------------------------------------")
+            output = f"./audio_extract/{rename}_extract.mp3"
+            s = f"ffmpeg -i {input} {output}"
+            cmd = s.split(" ")
+            subprocess.check_output(cmd)
+            video_ins.audio_extract = output
+            video_ins.save(update_fields = ['audio_extract'])
+
